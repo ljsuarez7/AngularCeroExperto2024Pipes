77b5f59c467fb8bd5ae95425501bf207ebea0aa4
Javier Suárez <ljsuarez7@gmail.com>
2024-06-29 16:52:01 +0200

    pipes personalizados

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,6 +5,8 @@ import { FieldsetModule } from 'primeng/fieldset';
 // import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
@@ -13,7 +15,9 @@ import { PanelModule } from 'primeng/panel';
     ButtonModule,
     CardModule,
     FieldsetModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule,
+    TableModule
   ]
 })
 export class PrimeNgModule { }
